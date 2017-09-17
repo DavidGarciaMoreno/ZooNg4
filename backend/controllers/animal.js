f9f2f147b32e202162117359c8f08ad20a898a97
David Garcia Moreno <david.garcia.dev@gmail.com>
2017-09-17 20:40:59 +0200

    Clase 58: Guardar animal

--- backend/controllers/animal.js
+++ backend/controllers/animal.js
@@ -15,6 +15,33 @@ function pruebas(req, res) {
 	});
 }
 
+function saveAnimal(req, res) {
+	var animal = new Animal();
+	var params = req.body;
+
+	if(params.name) {
+		animal.name = params.name;
+		animal.description = params.description;
+		animal.year = params.year;
+		animal.image = null;
+		animal.user = req.user.sub;
+		animal.save((err, animalStored) => {
+			if(err) {
+				res.status(500).send({ message: 'Error en el servidor' });
+			} else {
+				if(!animalStored) {
+					res.status(404).send({ animalmessage: 'No se ha guardado el animal' });
+				} else {
+					res.status(200).send({ animal: animalStored });
+				}
+			}
+		})
+	} else {
+		res.status(400).send({ message: 'El nombre del animal es obligatorio' });
+	}
+}
+
 module.exports = {
-	pruebas
+	pruebas,
+	saveAnimal
 };
